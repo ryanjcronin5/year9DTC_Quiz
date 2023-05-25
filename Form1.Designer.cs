53d867b8b6a1d29b5bd976f51bc14b108ec41eb3
Ryan Cronin <ryanjcronin5@gmail.com>
2023-05-25 12:47:59 +1200

    Initial styling for the panels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace year9DTC_Quiz
+﻿using year9DTC_Quiz.Properties;
+
+namespace year9DTC_Quiz
 {
     partial class Form1
     {
@@ -28,13 +30,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bodyPanel = new System.Windows.Forms.Panel();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.introText = new System.Windows.Forms.Label();
+            this.bodyPanel.SuspendLayout();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bodyPanel
+            // 
+            this.bodyPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.bodyPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bodyPanel.Controls.Add(this.introText);
+            this.bodyPanel.Controls.Add(this.headerPanel);
+            this.bodyPanel.Controls.Add(this.groupBox1);
+            this.bodyPanel.Location = new System.Drawing.Point(107, 44);
+            this.bodyPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.bodyPanel.Name = "bodyPanel";
+            this.bodyPanel.Size = new System.Drawing.Size(608, 353);
+            this.bodyPanel.TabIndex = 1;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.headerPanel.Controls.Add(this.headerLabel);
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(608, 70);
+            this.headerPanel.TabIndex = 0;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.headerLabel.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.headerLabel.Location = new System.Drawing.Point(0, 0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(608, 70);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Year 9 DTC Quiz";
+            this.headerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(82, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 160);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // introText
+            // 
+            this.introText.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introText.Location = new System.Drawing.Point(4, 88);
+            this.introText.Name = "introText";
+            this.introText.Padding = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.introText.Size = new System.Drawing.Size(601, 68);
+            this.introText.TabIndex = 1;
+            this.introText.Text = "Information and instructions for the students using this quiz.";
+            this.introText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::year9DTC_Quiz.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.bodyPanel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.Text = "DTC Beginner Quiz";
+            this.bodyPanel.ResumeLayout(false);
+            this.headerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel bodyPanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label introText;
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label headerLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace year9DTC_Quiz
         public Form1()
         {
             InitializeComponent();
+            bodyPanel.Paint += bodyPanel_Paint;
+        }
+        private GraphicsPath GetRoundedRectanglePath(Rectangle rect, int radius)
+        {
+            // Create a new GraphicsPath object to hold the rounded rectangle path
+            GraphicsPath roundedRect = new GraphicsPath();
+
+            // Add the arcs that define the top left and top right corners of the rectangle
+            roundedRect.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+            roundedRect.AddLine(rect.X + radius, rect.Y, rect.X + rect.Width - radius * 2, rect.Y);
+
+            // Add the arcs that define the bottom right and bottom left corners of the rectangle
+            roundedRect.AddArc(rect.X + rect.Width - radius * 2, rect.Y, radius * 2, radius * 2, 270, 90);
+            roundedRect.AddLine(rect.X + rect.Width, rect.Y + radius * 2, rect.X + rect.Width, rect.Y + rect.Height - radius * 2);
+            roundedRect.AddArc(rect.X + rect.Width - radius * 2, rect.Y + rect.Height - radius * 2, radius * 2, radius * 2, 0, 90);
+            roundedRect.AddLine(rect.X + rect.Width - radius * 2, rect.Y + rect.Height, rect.X + radius * 2, rect.Y + rect.Height);
+
+            // Add the arc that defines the top left corner of the rectangle and close the path
+            roundedRect.AddArc(rect.X, rect.Y + rect.Height - radius * 2, radius * 2, radius * 2, 90, 90);
+            roundedRect.AddLine(rect.X, rect.Y + rect.Height - radius * 2, rect.X, rect.Y + radius * 2);
+            roundedRect.CloseFigure();
+
+            // Return the GraphicsPath object that defines the rounded rectangle shape
+            return roundedRect;
+        }
+
+        private void bodyPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle rect = bodyPanel.ClientRectangle;
+            int radius = 30;
+            GraphicsPath path = GetRoundedRectanglePath(rect, radius);
+            bodyPanel.Region = new Region(path);
         }
     }
 }
